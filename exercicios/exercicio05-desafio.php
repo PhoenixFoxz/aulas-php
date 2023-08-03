286fe730bfc0ced9b4df22723efe28028bde2ee0
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-08-03 11:53:07 -0300

    Exercício 5

---
+++ exercicios/exercicio05-desafio.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 05</title>
+    <style>
+        .reprovado {
+            color: red;
+        }
+        .aprovado {
+            color: green;
+        }
+    </style>
+</head>
+<body>
+<h1>Exercício 5</h1>
+<hr>
+
+<?php 
+
+
+function calculoMedia($nota1, $nota2){
+    $media = ($nota1 + $nota2) / 2;
+    return $media;
+}
+
+function situacao($media){
+    if ($media >= 7){
+        $resultado = "<span class='aprovado'>Aprovado</span>";
+    } else {
+        $resultado = "<span class='reprovado'>Reprovado</span>";
+    }
+    return $resultado;
+}
+?>
+<p>Sua média é de <?=calculoMedia($nota1, $nota2)?>, sendo assim você está <?=situacao(calculoMedia($nota1, $nota2))?></p>
+<?php
+?>
+</body>
+</html>
