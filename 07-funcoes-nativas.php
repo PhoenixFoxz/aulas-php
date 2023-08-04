1920a4efed1d0a79697413450e0d51b2d1fbe24e
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-08-04 11:33:55 -0300

    Funções no PHP

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -19,11 +19,45 @@
 $nome = "   Vinicius me deve 80 paçocas   ";
 $nomeSemEspaco = trim($nome);
 ?>    
-<pre><?=var_dump($nome)?></pre>
-<pre><?=var_dump($nomeSemEspaco)?></pre>
+    <pre><?=var_dump($nome)?></pre>
+    <pre><?=var_dump($nomeSemEspaco)?></pre>
+
+    <h3>str_replace()</h3>
+<?php
+// Função que permite substituição
+// de strings/caracteres.
+$fraseFeia = "Fulano é um bobão xarope";
+$fraseBunitinha = str_replace(
+    ["xarope", "bobão"],
+    "******",
+    $fraseFeia
+    );
+?>
+    <p><?=$fraseFeia?></p>
+    <p><?=$fraseBunitinha?></p>
+    <h3>explode()</h3>
+<?php
+// Função que tranforma uma string em um array
+$linguagens = "HTML - CSS - JS - PHP";
+$arrayLinguagens = explode(" - ", $linguagens);
+?>
+    <pre><?=var_dump($linguagens)?></pre>
+    <pre><?=var_dump($arrayLinguagens)?></pre>   
+
+    <hr>
+
     <h2>Numéricas</h2>
+    <h3>min, max, round</h3>
+<?php
+$valores = [10, 5, 50, 1000, 275];
+$valorQualquer = 1259.45;
+?>
+    <p>Menor valor: <?=min($valores)?></p>
+    <p>Maior valor: <?=max($valores)?></p>
+    <p>Arredondamento: <?=round($valorQualquer)?></p>
     <h2>Arrays</h2>
     <h2>Filtros</h2>
     <h2>Segurança</h2>
 </body>
 </html>
+
