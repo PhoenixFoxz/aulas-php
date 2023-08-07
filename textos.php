116087fa7b750f394383a3665bb60ad024e93027
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-08-07 10:50:06 -0300

    Include e Required

---
+++ textos.php
@@ -0,0 +1,8 @@
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, asperiores.</p>
+<p>Illum magnam nesciunt iusto nulla asperiores esse totam quae suscipit.</p>
+<p>Eos, repellendus sequi voluptatibus harum voluptas perspiciatis alias? In, sunt.</p>
+<p>Repellat, magni tenetur? Facere aspernatur, modi optio veniam iure nemo.</p>
+
+<?php 
+include "lista.html"
+?>
