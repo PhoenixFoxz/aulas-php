d897749647816fdb0b108a34edca17d38a984eee
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-08-08 12:01:52 -0300

    Empty e Isset

--- processa-post.php
+++ processa-post.php
@@ -22,16 +22,33 @@ if(empty($_POST["nome"]) || empty($_POST["email"])){
 
 $nome = $_POST["nome"];
 $email = $_POST["email"];
-$mensagem = $_POST["mensagem"]
+$mensagem = $_POST["mensagem"];
+// Se houver interesses (ou seja, foi selecionado pelo menos 1), guarde na variável o $_POST["interesses"]. Caso contrário, guarde na variável um array vazio.
+$interesses = $_POST["interesses"];
+$idade = $_POST["idade"]
 ?>
 
     <h2>Dados:</h2>
     <ul>
         <li>Nome: <?=$nome?></li>
         <li>E-mail: <?=$email?></li>
-        <li>Mensagem: <?=$mensagem?></li>
+        <li>Idade: <?=$idade?></li>
+
+<?php
+// Se a variável mensagem NÃO ESTIVER VAZIA, mostre o <li> com a mensagem
+if(!empty($mensagem)){
+?>
+       <li>Mensagem: <?=$mensagem?></li>
+<?php
+}
+?>
     </ul>
 <?php
+if(isset($interesses)){
+?>
+       <li>Interesses: <?=implode(",", $interesses)?></li>
+<?php
+} 
 }
 ?>
 </body>
